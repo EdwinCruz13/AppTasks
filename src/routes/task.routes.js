7fb5e181688efa4e79e433334650ee8c8dd57be3
edwin.cruz <edwin.cruz@EDWIC>
2023-06-26 18:49:16 -0600

    adding the middlaware in the routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,20 +1,23 @@
 import { Router } from "express";
-import { getTasks, getTask, createTask, updateTask, deleteTask } from "../controllers/task.controllers.js"
+import { getTasks, getTask, createTask, updateTask, deleteTask } from "../controllers/task.controllers.js";
+
+//import access athorization middleware
+import { UserAuth } from "../middleware/auth.validate.middleware.js";
 
 //define a new router
 const router = Router();
 
 
 //create http path request
-router.get("/getTasks", getTasks);
+router.get("/getTasks", UserAuth, getTasks);
 
-router.get("/getTask/:id", getTask);
+router.get("/getTask/:id", UserAuth, getTask);
 
-router.post("/createTask", createTask);
+router.post("/createTask", UserAuth, createTask);
 
-router.put("/updateTask/:id", updateTask);
+router.put("/updateTask/:id", UserAuth, updateTask);
 
-router.delete("/deleteTask/:id", deleteTask);
+router.delete("/deleteTask/:id", UserAuth, deleteTask);
 
 
 
